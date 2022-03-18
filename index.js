6753a8c4ff709bb867e175d133f511888361b7e5
bluuweb <contacto@bluuweb.cl>
2022-03-17 21:37:11 -0300

    comentarios

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 
+// simple database
+const { results } = require("./data/usuarios");
+
+// habilitar req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// archivos estáticos
 app.use(express.static(__dirname + "/public"));
 
-const { results } = require("./data/usuarios");
+// middleware auth
+const requireAuth = (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization.split(" ")[1];
 
+    if (!token) return res.status(403).json({ msg: "no existe el token" });
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = payload;
+        next();
+    } catch (error) {
+        // console.log(error);
+        if (error.message === "jwt expired") {
+            return res.status(403).json({ msg: "expirado token" });
+        }
+        return res.status(401).json({ msg: "token no válido" });
+    }
+};
+
+// acceso user
 app.post("/login", (req, res) => {
     const { email, password } = req.body;
 
@@ -29,30 +52,15 @@ app.post("/login", (req, res) => {
     return res.json({ token });
 });
 
-const requireAuth = (req, res, next) => {
-    const { authorization } = req.headers;
-    const token = authorization.split(" ")[1];
-
-    if (!token) return res.status(403).json({ msg: "no existe el token" });
-    try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = payload;
-        next();
-    } catch (error) {
-        // console.log(error);
-        if (error.message === "jwt expired") {
-            return res.status(403).json({ msg: "expirado token" });
-        }
-        return res.status(401).json({ msg: "token no válido" });
-    }
-};
-
+// render perfil.html
 app.get("/perfil", (req, res) => {
     res.sendFile(__dirname + "/public/perfil.html");
 });
 
+// información protegida
 app.post("/perfil", requireAuth, (req, res) => {
     res.json({ msg: "ruta protegida", user: req.user });
 });
 
+// server
 app.listen(5000, console.log("andando ❤"));
